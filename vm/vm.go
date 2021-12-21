e3a58db38ffc957dd7b97eb24a341051dc15e40a
Sergio Costa <sergio.costa@ufma.br>
2021-12-21 10:19:02 -0300

    adding translator

--- vm/vm.go
+++ vm/vm.go
@@ -26,6 +26,67 @@ func Interpret(path string) {
 	vm.Run()
 }
 
+func writePush(value string) {
+	arg, err := strconv.Atoi(value)
+	if err != nil { // é uma variavel
+		fmt.Println(fmt.Sprintf("@%s", value))
+		fmt.Println("D=M")
+		fmt.Println("@SP")
+		fmt.Println("A=M")
+		fmt.Println("M=D")
+		fmt.Println("@SP")
+		fmt.Println("M=M+1")
+	} else {
+		fmt.Println(fmt.Sprintf("@%d", arg))
+		fmt.Println("D=A")
+		fmt.Println("@SP")
+		fmt.Println("A=M")
+		fmt.Println("M=D")
+		fmt.Println("@SP")
+		fmt.Println("M=M+1")
+	}
+
+}
+
+func Translator(path string) {
+	dat, _ := ioutil.ReadFile(path)
+	instructions := strings.Split(string(dat), "\n")
+	vm := New(instructions)
+	for vm.pc < len(vm.instructions) {
+		inst := vm.instructions[vm.pc]
+		switch inst {
+		case "push":
+			nextInst := vm.instructions[vm.pc+1]
+			writePush(nextInst)
+			vm.pc++
+
+		case "add", "mul":
+			arg1 := vm.stackPop()
+			arg2 := vm.stackPop()
+			if inst == "add" {
+				vm.stackPush(arg1 + arg2)
+			} else {
+				vm.stackPush(arg1 * arg2)
+			}
+
+		case "pop":
+			arg := vm.instructions[vm.pc+1]
+			vm.st[arg] = vm.stackPop()
+			vm.pc++
+
+		case "print":
+			fmt.Println(vm.stackPop())
+		}
+		vm.pc++
+		/*
+			fmt.Print("Stack:")
+			fmt.Println(vm.stack)
+			fmt.Print("Symbol table:")
+			fmt.Println(vm.st)
+		*/
+	}
+}
+
 func (vm *VM) stackPop() int {
 	sp := len(vm.stack)
 	value := vm.stack[sp-1]
